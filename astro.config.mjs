f6e87d125703d4dbe092bded284169cb459f6365
bennygoldman <goldmanb@gmail.com>
2023-12-11 23:35:43 -0500

    complete astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://splendorous-melba-abbd29.netlify.app/blog/"
+  site: "https://splendorous-melba-abbd29.netlify.app/blog/",
+  integrations: [preact()]
 });
